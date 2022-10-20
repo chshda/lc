9a5d7ebaf61021c146c2f2d9e1dc279596cb4359
shengda <chshda09@gmail.com>
2022-10-20 22:27:14 +0800

    tem

---
+++ temp/header.h
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define repin(i, a, b) for (int i = (a); i <= (b); i++)
+#define ms(a, n) memset(a, n, sizeof(a))
+
+const int N = 2e5;
