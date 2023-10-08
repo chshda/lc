3b29b1ce68e2d9202b8d0192ee89558a0f51d13f
shengda <chshda09@gmail.com>
2023-10-08 16:56:18 +0800

    Update settign

--- test/reset.cpp
+++ test/reset.cpp
@@ -2,31 +2,22 @@
 using namespace std;
 
 #ifdef LOCAL_PROJECT
-#define debug(x) cerr << #x << ": " << x << endl
-#else
-#define debug(...) 42
+#include "local.h"
 #endif
 
 using ll = long long;
 using pii = pair<int, int>;
 
 void solve(int tc) {
-    cout << "hello world!" << endl;
+    
 }
 
 int32_t main() {
-#ifdef LOCAL_PROJECT
-    freopen("data/in.txt", "r", stdin);
-    freopen("data/out.txt", "w", stdout);
-    freopen("data/error.txt", "w", stderr);
-#endif
-
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int tcn;
-    cin >> tcn;
-    for (int tc = 1; tc <= tcn; tc++) { debug(tc); solve(tc); }
+    int tcn; cin >> tcn;
+    for (int tc = 1; tc <= tcn; tc++) solve(tc);
 
     return 0;
 }
